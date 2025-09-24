c2e9335b936e296d405f7bf9c3b3338dcab4335b
Alexis Condori Rivera <aecondorir@unjbg.edu.pe>
2025-09-24 01:06:45 -0500

    add revision_historia

---
+++ scripts/dml/historia_clinica/revision_historia.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE PROCEDURE i_revision_historia(
+    IN p_id_historia UUID,
+    IN p_id_docente UUID,
+    IN p_estado estado_revision_enum,
+    IN p_observaciones TEXT DEFAULT NULL
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO revisiones_historia (id_historia, id_docente, estado, observaciones)
+    VALUES (p_id_historia, p_id_docente, p_estado, p_observaciones);
+
+    RAISE NOTICE 'Revision de historia registrada exitosamente para historia % por docente %.', p_id_historia, p_id_docente;
+
+EXCEPTION
+    WHEN foreign_key_violation THEN
+        RAISE EXCEPTION 'El id_historia % o el id_docente % no existen.', p_id_historia, p_id_docente;
+    WHEN others THEN
+        RAISE EXCEPTION 'Ocurrio un error al registrar la revision: %', SQLERRM;
+END;
+$$;
+
