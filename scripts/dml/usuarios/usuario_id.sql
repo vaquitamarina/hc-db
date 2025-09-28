759f32589b6d7ae32410c86c2e4e9419c37cfc45
vaquita <cristhianvm55@gmail.com>
2025-09-28 01:23:45 -0500

    add getuserbyid

---
+++ scripts/dml/usuarios/usuario_id.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION fn_obtener_usuario(
+    p_id_usuario UUID
+)
+RETURNS TABLE (
+    id_usuario    UUID,
+    usuario_login VARCHAR,
+    nombre        VARCHAR,
+    apellido      VARCHAR,
+    dni           VARCHAR,
+    email         VARCHAR
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT u.id_usuario,
+           u.usuario_login,
+           u.nombre,
+           u.apellido,
+           u.dni,
+           u.email
+    FROM usuarios u
+    WHERE u.id_usuario = p_id_usuario;
+END;
+$$ LANGUAGE plpgsql;
+
